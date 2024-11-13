6dc06ee886f68538aa46065f0ef0e6793dd1145f
wonnn-kim <won.kim@digipen.edu>
2024-11-13 19:53:44 +0900

    dd

--- Manzo/Manzo/Game/Mode1.cpp
+++ Manzo/Manzo/Game/Mode1.cpp
@@ -53,7 +53,7 @@ void Mode1::Load() {
     GetGSComponent<CS230::GameObjectManager>()->Add(ship_ptr);
 
     //// camera
-    Math::rect Boundary({ -640,-540 }, { 640,540 });
+    Math::rect Boundary({ -640,-370 }, { 640,360 });
     camera = new CS230::Cam();
     AddGSComponent(camera);
     camera->SetLimit(Boundary);
@@ -74,12 +74,13 @@ void Mode1::Load() {
 
 
     GetGSComponent<Background>()->Add("assets/images/background/temp_background.png", 0.0f);
-    /*GetGSComponent<Background>()->Add("assets/images/background/bg1.png", 0.3f);
+    GetGSComponent<Background>()->Add("assets/images/background/bg1.png", 0.3f);
     GetGSComponent<Background>()->Add("assets/images/background/bg2.png", 0.4f);
     GetGSComponent<Background>()->Add("assets/images/background/bg3.png", 0.5f);
     GetGSComponent<Background>()->Add("assets/images/background/bg4.png", 0.6f);
-    GetGSComponent<Background>()->Add("assets/images/background/bg5.png", 0.7f);*/
-    GetGSComponent<Background>()->Add("assets/images/background/bg_rock4.png", 0.4f);/*
+    GetGSComponent<Background>()->Add("assets/images/background/bg5.png", 0.7f);
+    /*
+    GetGSComponent<Background>()->Add("assets/images/background/bg_rock4.png", 0.4f);
     GetGSComponent<Background>()->Add("assets/images/background/bg_rock2.png", 0.5f);
     GetGSComponent<Background>()->Add("assets/images/background/bg_rock1.png", 0.7f);*/
 
