6caaceb8da9d30936df4c010645390a324f7770d
wonnn-kim <won.kim@digipen.edu>
2024-11-29 00:03:09 +0900

    delete magic number

--- Manzo/Manzo/Game/Ship.cpp
+++ Manzo/Manzo/Game/Ship.cpp
@@ -122,21 +122,23 @@ void Ship::Update(double dt)
     GameObject::Update(dt);
     CS230::RectCollision* collider = GetGOComponent<CS230::RectCollision>();
 
+    float width = 1280;
+    float height = 720;
     // World Boundary
-    if (collider->WorldBoundary_rect().Left() < Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x - 1280) {
-        UpdatePosition({ Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x - 1280 - collider->WorldBoundary_rect().Left(), 0});
+    if (collider->WorldBoundary_rect().Left() < Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x - width) {
+        UpdatePosition({ Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x - width - collider->WorldBoundary_rect().Left(), 0});
         SetVelocity({ 0, GetVelocity().y });
     }
-    if (collider->WorldBoundary_rect().Right() > Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x + 1280) {
-        UpdatePosition({ Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x + 1280 - collider->WorldBoundary_rect().Right(), 0 });
+    if (collider->WorldBoundary_rect().Right() > Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x + width) {
+        UpdatePosition({ Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().x + width - collider->WorldBoundary_rect().Right(), 0 });
         SetVelocity({ 0, GetVelocity().y });
     }
-    if (collider->WorldBoundary_rect().Bottom() < Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y - 720) {
-        UpdatePosition({ 0, Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y - 360 - collider->WorldBoundary_rect().Bottom() });
+    if (collider->WorldBoundary_rect().Bottom() < Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y - height) {
+        UpdatePosition({ 0, Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y - height - collider->WorldBoundary_rect().Bottom() });
         SetVelocity({ GetVelocity().x, 0 });
     }
-    if (collider->WorldBoundary_rect().Top() > Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y + 360) {
-        UpdatePosition({ 0, Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y + 360 - collider->WorldBoundary_rect().Top()});
+    if (collider->WorldBoundary_rect().Top() > Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y + height) {
+        UpdatePosition({ 0, Engine::GetGameStateManager().GetGSComponent<CS230::Cam>()->GetPosition().y + height - collider->WorldBoundary_rect().Top()});
         SetVelocity({ GetVelocity().x, 0 });
     }
 
