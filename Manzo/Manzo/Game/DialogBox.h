b81103e5186021ba824e83f1c29adf2b66d0c0ed
Seohyeon-Min <seohyeon.min@digipen.edu>
2024-12-10 04:32:26 +0900

    why

--- Manzo/Manzo/Game/DialogBox.h
+++ Manzo/Manzo/Game/DialogBox.h
@@ -7,41 +7,33 @@
 #include "../Engine/Component.h"
 #include "Fish.h"
 #include "Skill.h"
+#include "../Engine/Rapidjson.h"
 
 class Dialog : public Component
 {
 public:
 
-	Dialog();
-	void Update(double dt); // maybe not used dt..
-	void Buy(Skillsys::Skill_list skill, int input); //Skill = which player's buy? input = How much use money for that skill?
-	void Shop_Back_draw();
-	void Shop_icon_draw();
-	void Shop_button_draw();
-	void Inventory_Back_draw();
-	void Inventory_Icon_draw();
+    Dialog();
+    void LoadDialog(int id, double typingSpeed);
+    void Update(double dt);
+    void Draw();
+    void Unload();
 
 
 private:
+    GLTexture* DialogBox = nullptr;
+    vec2 boxposition;
+    mat3 back_matrix;
 
-	Skillsys* skill_ptr;
-	int Net_Money = 3;
-	bool shop_available = false; // false = shop disappear / true = shop appear
-	GLTexture* shop_background = nullptr; //texture
-	GLTexture* shop_icon = nullptr; //texture
-	GLTexture* shop_button = nullptr; //texture
-	shop_list pick = First; //player's pick
-	mat3 back_matrix; // shop background matrix
-	mat3 inv_back_matrix; // inventory background matrix
-	mat3 botton_matrix; // shop botton matrix
-	vec2 base_botton_direction;
-	vec2 defualt_botton_direction;
-	vec2 inven_back_pos;
-	DrawCall draw_call;
-	std::vector<mat3> icon_matrix;
-	std::vector<mat3> inv_icon_matrix;
-	std::vector<DrawCall> icon_draw_calls;
-	std::vector<DrawCall> inv_icon_draw_calls; //for inventory icon
-	std::vector<vec2> icon_direction;
+    DrawCall draw_call;
 
+    JsonParser_dialog* dialog;
+    std::string fullText;
+    std::string displayedText;
+    std::string character;
+    std::string idToString;
+    size_t currentIndex;
+    double elapsedTime;
+    double typingSpeed;
+    bool IsTyping;
 };
